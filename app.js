3c4dfecde1ae3112fefca892814a6d17103949c4
Mingrow <nikitosvozh@gmail.com>
2024-06-03 19:40:13 +0300

    ex: project calculation

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const hourlyRate = 80;
+const availableDays = 11 - 2;
+const needDays = 40 / 5;
+if (availableDays >= needDays) {
+    let payment = hourlyRate * 40;
+    console.log(`True: ${payment}`)
+} else {
+    console.log(`False`)
+}
